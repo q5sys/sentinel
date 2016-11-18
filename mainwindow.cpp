eeef74121e6b771b05ec79aa36c8a0914fdd369c
q5sys <jt@obs-sec.com>
2016-11-18 13:54:29 -0500

    building proc list structure

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,41 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+void MainWindow::addProc(){
+//  Host proc(ui->line_conn_ip->text(), ui->line_conn_name->text(), ui->line_conn_user->text(), ui->line_conn_pass->text());
+//  PROCS << proc;
+  updateProcs();
+}
+//PROCLIST FORMAT
+// PID, USER, ROUTE, DEVICE, SENT, RECEIVED
+
+
+void MainWindow::updateProcs(){
+  ui->tree_procs->clear();
+for(int i=0; i<PROCS.length(); i++){
+    QTreeWidgetItem *it = new QTreeWidgetItem(ui->tree_procs);
+
+    it->setText(0, PROCS[i].pid());
+    it->setText(1, PROCS[i].user());
+    it->setText(2, PROCS[i].route());
+    it->setText(3, PROCS[i].device());
+    it->setText(4, PROCS[i].out());
+    it->setText(5, PROCS[i].in());
+    ui->tree_procs->addTopLevelItem(it);
+  }
+
+  //sort the tree widget
+  for(int i=0; i < ui->tree_procs->columnCount(); i++){
+    ui->tree_procs->resizeColumnToContents(i);
+  }
+}
+
+void MainWindow::getActiveConnections(){;}
+void MainWindow::getPID(){;}
+void MainWindow::getProcessName(){;}
+void MainWindow::getRoute(){;}
+void MainWindow::getDevice(){;}
+void MainWindow::sumOut(){;}
+void MainWindow::sumIn(){;}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
+#include <QList>
+
 
 namespace Ui {
 class MainWindow;
@@ -14,9 +17,52 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+//    static QString BytesToDisplaySize(qint64 ibytes);
+    void addProc();
+    void updateProcs();
+    void getActiveConnections();
+    void getPID();
+    void getProcessName();
+    void getRoute();
+    void getDevice();
+    void sumOut();
+    void sumIn();
+
 
 private:
     Ui::MainWindow *ui;
 };
 
 #endif // MAINWINDOW_H
+
+
+/*
+ * net.inet.tcp.pcblist
+
+#define	KERN_IPC_MB_STAT	"kern.ipc.mb_stat"
+#include <sys/sysctl.h>
+*/
+class Proc{
+private:
+        QString i_pid, i_user, i_route, i_device, i_out, i_in; //internal vars
+public:
+        Proc(QString pid="", QString user="", QString route="", QString device="", QString out="", QString in=""){
+          i_pid=pid; i_user=user; i_route=route; i_device=device; i_out=out; i_in=in;
+        }
+        ~Proc(){
+          i_pid.clear(); i_user.clear(); i_route.clear(); i_device.clear(); i_out.clear(); i_in.clear();
+        }
+        //Main info functions
+        const QString pid() const { return i_pid; }
+        const QString user() const { return i_user; }
+        const QString route() const { return i_route; }
+        const QString device() const { return i_device; }
+        const QString out() const { return i_out; }
+        const QString in() const { return i_in; }
+
+};
+
+//General class for loading/saving/finding proc information
+class ProcList : public QList<Proc>{
+};
+extern ProcList PROCS;
